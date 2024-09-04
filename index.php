b44f184f56a49917ac28bbc095ee74cb230d8f93
Klee Collage <klee.collage@gmail.com>
2024-09-04 15:30:04 -0500

    Page layout
    styles implemented

--- index.php
+++ index.php
@@ -41,6 +41,7 @@
                 <!-- --------------------   BARRA LATERAL   -------------------- -->
                 <aside id="lateral">
                     <div id="login" class="block_aside">
+                        <h3>Entrar a la web</h3>
                         <form action="#" method="post">
                             <label for="email">Email</label>
                             <input type="email" name="email" />
@@ -50,28 +51,33 @@
 
                             <input type="submit" value="Enviar" />
                         </form>
-                        <a href="#">Mis Pedidos</a>
-                        <a href="#">Gestionar Pedidos</a>
-                        <a href="#">Gestionar Categorias</a>
+                        <ul>
+                            <li><a href="#">Mis Pedidos</a></li>
+                            <li><a href="#">Gestionar Pedidos</a></li>
+                            <li><a href="#">Gestionar Categorias</a></li>
+                        </ul>
                     </div>
                 </aside>
                 <!-- --------------------   CONTENIDO CENTRAL   -------------------- -->
                 <div id="central">
+                    <h1>Productos Destacados</h1>
                     <div class="product">
                         <img src="assets/img/logo.png" alt="logo">
                         <h2>Camiseta Blanca Ajustada</h2>
                         <p>30 euros</p>
-
-                        <a href="#">Comprar</a>
+                        <a href="#" class="button">Comprar</a>
+                    </div>
+                    <div class="product">
                         <img src="assets/img/logo.png" alt="logo">
                         <h2>Camiseta Blanca Ajustada</h2>
                         <p>30 euros</p>
-                        <a href="#">Comprar</a>
-
+                        <a href="#" class="button">Comprar</a>
+                    </div>
+                    <div class="product">
                         <img src="assets/img/logo.png" alt="logo">
                         <h2>Camiseta Blanca Ajustada</h2>
                         <p>30 euros</p>
-                        <a href="#">Comprar</a>
+                        <a href="#" class="button">Comprar</a>
                     </div>
                 </div>
             </div>
